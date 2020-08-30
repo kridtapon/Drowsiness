09f13cb79be884b7bb600a4c70ff4fb27f650116
kridtapon <51466568+kridtapon@users.noreply.github.com>
2020-08-30 09:34:55 +0700

    Add files via upload

---
+++ face_recognition and drowsiness_Detection_test2.py
@@ -0,0 +1,88 @@
+import cv2
+import numpy as np
+from scipy.spatial import distance
+from imutils import face_utils
+import imutils
+import dlib
+import face_recognition
+
+video_capture = cv2.VideoCapture(0)
+ken_image = face_recognition.load_image_file("ken.jpg")
+ken_face_encoding = face_recognition.face_encodings(ken_image)[0]
+
+whan_image = face_recognition.load_image_file("whan.jpg")
+whan_face_encoding = face_recognition.face_encodings(whan_image)[0]
+known_face_encodings = [
+    ken_face_encoding,
+    whan_face_encoding
+]
+known_face_names = [
+    "ken",
+    "Namwhan"
+]
+def eye_aspect_ratio(eye):
+	A = distance.euclidean(eye[1], eye[5])
+	B = distance.euclidean(eye[2], eye[4])
+	C = distance.euclidean(eye[0], eye[3])
+	ear = (A + B) / (2.0 * C)
+	return ear
+
+thresh = 0.25
+frame_check = 20
+detect = dlib.get_frontal_face_detector()
+predict = dlib.shape_predictor("D:\\drive C\\Drowsiness_Detection-master\\Drowsiness_Detection-master\\shape_predictor_68_face_landmarks.dat")# Dat file is the crux of the code
+(lStart, lEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["left_eye"]
+(rStart, rEnd) = face_utils.FACIAL_LANDMARKS_68_IDXS["right_eye"]
+flag=0
+while True:
+    ret, frame = video_capture.read()
+    rgb_frame = frame[:, :, ::-1]
+    face_locations = face_recognition.face_locations(rgb_frame)
+    face_encodings = face_recognition.face_encodings(rgb_frame, face_locations)
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    subjects = detect(gray, 0)
+    for (top, right, bottom, left), face_encoding in zip(face_locations, face_encodings):
+        matches = face_recognition.compare_faces(known_face_encodings, face_encoding)
+        name = "Unknown"
+        face_distances = face_recognition.face_distance(known_face_encodings, face_encoding)
+        best_match_index = np.argmin(face_distances)
+        if matches[best_match_index]:
+            name = known_face_names[best_match_index]
+        cv2.rectangle(frame, (left, top), (right, bottom), (0, 0, 255), 2)
+        cv2.rectangle(frame, (left, bottom - 35), (right, bottom), (0, 0, 255), cv2.FILLED)
+        font = cv2.FONT_HERSHEY_DUPLEX
+        cv2.putText(frame, name, (left + 6, bottom - 6), font, 1.0, (255, 255, 255), 1)
+    cv2.imshow('Video', frame)
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+    for subject in subjects:
+            shape = predict(gray, subject)
+            shape = face_utils.shape_to_np(shape)#converting to NumPy Array
+            leftEye = shape[lStart:lEnd]
+            rightEye = shape[rStart:rEnd]
+            leftEAR = eye_aspect_ratio(leftEye)
+            rightEAR = eye_aspect_ratio(rightEye)
+            ear = (leftEAR + rightEAR) / 2.0
+            leftEyeHull = cv2.convexHull(leftEye)
+            rightEyeHull = cv2.convexHull(rightEye)
+            cv2.drawContours(frame, [leftEyeHull], -1, (0, 255, 0), 1)
+            cv2.drawContours(frame, [rightEyeHull], -1, (0, 255, 0), 1)
+            if ear < thresh:
+                    flag += 1
+                    print (flag)
+                    if flag >= frame_check:
+                            cv2.putText(frame, "**************************ALERT!**************************", (0, 30),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                            cv2.putText(frame, "**************************ALERT!**************************", (0,470),cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 2)
+                            #print ("Drowsy")
+
+            else:
+                    flag = 0
+    cv2.imshow("Frame", frame)
+    key = cv2.waitKey(1) & 0xFF
+
+    if key == ord("q"):
+            break
+video_capture.release()
+cv2.destroyAllWindows()
+cap.stop()
+
